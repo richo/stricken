b2f0f5543f8ed807dcb5b4b1abe3a3d6b6ff850a
Richo Healey <richo@psych0tik.net>
2013-05-18 19:47:52 +1000

    Minimal first cut of the frontend

--- stricken/__init__.py
+++ stricken/__init__.py
@@ -4,4 +4,8 @@ from flask import Flask, render_template, request
 def make_stricken(station):
     app = Flask(__name__)
 
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
     return app
