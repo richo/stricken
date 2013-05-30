3d8ecbf0cad94d858c108fd3452789833d7cb251
Richo Healey <richo@psych0tik.net>
2013-05-30 22:35:34 +1000

    Write a gref tip out for the message

--- create_thread.py
+++ create_thread.py
@@ -12,14 +12,22 @@ from groundstation.node import Node
 from groundstation.station import Station
 from groundstation.objects.root_object import RootObject
 from groundstation.objects.update_object import UpdateObject
+from groundstation.gref import Gref
 
 node = Node()
 station = Station.from_env(node)
 
-root = RootObject(str(uuid.uuid1()), "messages", stricken.PROTOCOL)
+CHANNEL = "messages"
+
+thread_id = str(uuid.uuid1())
+gref = Gref(station.store, CHANNEL, thread_id)
+
+root = RootObject(thread_id, CHANNEL, stricken.PROTOCOL)
 oid = station.write(root.as_object())
 print("Root id: %s" % (oid))
 
 update = UpdateObject([oid], "Post content")
 oid = station.write(update.as_object())
 print("Update id: %s" % (oid))
+
+gref.write_tip(oid, "")
